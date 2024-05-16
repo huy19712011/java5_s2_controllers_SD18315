57f0c7dbc5779c9b5850d832b02a9dd147ef29fe
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-05-16 19:08:56 +0700

    update

--- src/main/java/com/example/java5_s2_controllers_sd18315/controllers/BasicController.java
+++ src/main/java/com/example/java5_s2_controllers_sd18315/controllers/BasicController.java
@@ -3,19 +3,22 @@ package com.example.java5_s2_controllers_sd18315.controllers;
 import com.example.java5_s2_controllers_sd18315.entities.Student;
 import com.example.java5_s2_controllers_sd18315.services.StudentService;
 import jakarta.annotation.Nullable;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
+@RequestMapping(value = "/basic")
 public class BasicController {
 
     final
@@ -34,6 +37,7 @@ public class BasicController {
     }
 
 
+    //2. @RequestParam
     //@GetMapping(value = "/hello")
     //@ResponseBody
     ////public String sayHello(@RequestParam(name = "username") @Nullable String name) {
@@ -80,9 +84,67 @@ public class BasicController {
     @ResponseBody
     public String sayHelloV2c(
             @RequestParam Map<String, String> allParams
-            ) {
+    ) {
         return "Parameters: " + allParams.entrySet();
+    }
+
+    //3. @PathVariable
+    // http://localhost:8080/hello/1/email
+    @GetMapping(value = "/hello/{id}/{email}")
+    @ResponseBody
+    public String sayHelloV3a(
+            @PathVariable("id") int id,
+            @PathVariable("email") String email
+    ) {
+        return "Hello student with id = " + id + " email = " + email;
+    }
+
+    //4. @RequestMapping on Class level
+
+    //5. @RequestMapping with headers
+    @RequestMapping(value = "/method", headers = {"key1=value1", "id=1"})
+    @ResponseBody
+    public String method() {
+        return "method";
+    }
+
+    //6. @RequestMapping with produces, consumes
+    @RequestMapping(value = "/method6", produces = {"application/json", "application/xml"}, consumes = "text/html")
+    @ResponseBody
+    public String method6() {
+        return "method6";
+    }
+
+    //7. @CookieValue
+    @RequestMapping(value = "/readCookie")
+    @ResponseBody
+    public String readCookieValue(@CookieValue(value = "username", defaultValue = "defaultName") String username) {
+        return "Cookie value: " + username;
+    }
+
+    @GetMapping(value = "/change-username")
+    @ResponseBody
+    public String setCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("username", "ABC");
+        response.addCookie(cookie);
+
+        return "Usename is changed...";
+    }
+
+    @GetMapping(value = "/all-cookies")
+    @ResponseBody
+    public String readAllCookies(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+
+            return Arrays.stream(cookies)
+                    .map(c -> c.getName() + " = " + c.getValue())
+                    .collect(Collectors.joining(", "));
+        }
+
+        return "No cookies";
     }
 
 
 }
+
